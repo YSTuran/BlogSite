e8a9953f2cf16aed19b196d660d506d18c51a177
Yusuf S. Turan <siryusufsus@gmail.com>
2025-07-30 15:39:49 +0300

    Kullanıcı arayüzü hazırlandı

--- app/src/main/java/yusufs/turan/blogsite/model/Gonderi.kt
+++ app/src/main/java/yusufs/turan/blogsite/model/Gonderi.kt
@@ -1,3 +1,3 @@
 package yusufs.turan.blogsite.model
 
-data class Gonderi(val email : String, val yorum : String, val gorselUrl : String)
+data class Gonderi(val email : String, val baslik : String, val yorum : String, val gorselUrl : String)
