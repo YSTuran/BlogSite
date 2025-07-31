1088db171165b298defa0155d10e20bddd5fe4d2
Yusuf S. Turan <siryusufsus@gmail.com>
2025-07-31 10:31:11 +0300

    Kullanıcı giriş ekranı tamamlandı

--- app/src/main/java/yusufs/turan/blogsite/view/KullaniciFragment.kt
+++ app/src/main/java/yusufs/turan/blogsite/view/KullaniciFragment.kt
@@ -5,24 +5,84 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import yusufs.turan.blogsite.R
+import yusufs.turan.blogsite.databinding.FragmentKullaniciBinding
 
 
 class KullaniciFragment : Fragment() {
+    private var _binding : FragmentKullaniciBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kullanici, container, false)
+        _binding = FragmentKullaniciBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.kayitButton.setOnClickListener { kaydol(it) }
+        binding.girisButton.setOnClickListener { girisYap(it) }
 
+        val aktifKullanici = auth.currentUser
+        if (aktifKullanici != null){
+            //daha önce giriş yapılmış
+            val action =KullaniciFragmentDirections.actionKullaniciFragmentToFeedFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+    }
+
+    fun kaydol(view: View){
+        val email = binding.mailEditText.text.toString()
+        val password = binding.sifreEditText.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Kullanıcı Oluışturuldu
+                    val action = KullaniciFragmentDirections.actionKullaniciFragmentToFeedFragment()
+                    Navigation.findNavController(view).navigate(action)
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(requireContext(), exception.localizedMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+    fun girisYap(view: View){
+        val email = binding.mailEditText.text.toString()
+        val password = binding.sifreEditText.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener { task ->
+                // Giriş Yapıldı
+                val action = KullaniciFragmentDirections.actionKullaniciFragmentToFeedFragment()
+                Navigation.findNavController(view).navigate(action)
+            }.addOnFailureListener { exception ->
+                Toast.makeText(requireContext(), exception.localizedMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
